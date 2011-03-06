4ffabba9bf4489a21454fa69178048b76c7317da
5Keyem <5Keyem@gmail.com>
2011-03-06 19:21:26 +0100

    basic structure with tests

---
+++ tests/cz/muni/fi/pv168/CustomerTest.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pv168;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: fivekeyem
+ * Date: 3/6/11
+ */
+public class CustomerTest extends TestCase {
+}
