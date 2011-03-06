fa35ca4ac4784acfc1a4ee6d04ee2973f5b7cf38
5Keyem <5Keyem@gmail.com>
2011-03-06 19:07:43 +0100

    basic structure

---
+++ src/cz/muni/fi/pv168/Customer.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pv168;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: fivekeyem
+ * Date: 3/6/11
+ */
+public class Customer {
+
+    private int id;
+    private String name;
+}
