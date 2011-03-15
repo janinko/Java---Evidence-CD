736a4d29aa7cc833695b1ec5c367f37ee2718589
Jan Brázdil <janinko.g@gmail.com>
2011-03-15 10:06:02 +0100

    Base test structure

---
+++ tests/cz/muni/fi/pv168/CDManagerImplTest.java
@@ -0,0 +1,49 @@
+package cz.muni.fi.pv168;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: janinko
+ * Date: 15.3.11
+ * Time: 10:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class CDManagerImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateCD() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteCD() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateCD() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllCD() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCDByID() throws Exception {
+
+    }
+}
