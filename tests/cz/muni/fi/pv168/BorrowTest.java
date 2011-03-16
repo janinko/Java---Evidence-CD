6843d27aa6b1e94dc36b868c52b26b083241fb52
5Keyem <5Keyem@gmail.com>
2011-03-16 15:38:48 +0100

    basic Borrows test

--- tests/cz/muni/fi/pv168/BorrowTest.java
+++ tests/cz/muni/fi/pv168/BorrowTest.java
@@ -13,32 +13,31 @@ public class BorrowTest {
 
     @Test
     public void testBorrowEquals() {
-        Borrow borrow1;
-        Borrow borrow2;
         CD cd1 = new CD(1, "The Album", 2011);
         CD cd2 = new CD(2, "The Album 2", 2011);
         Customer customer1 = new Customer(1, "User Name");
         Customer customer2 = new Customer(2, "User Name 2");
+        Borrow borrow1 = new Borrow(1, cd1, customer1, true);
+        Borrow borrow2 = new Borrow(2, cd2, customer2, false);
 
-
-        borrow1 = new Borrow(1, cd1, customer1, true);
-        borrow2 = new Borrow(2, cd2, customer2, false);
-
+        // basic comparing
         assertFalse(borrow1.equals(null));
         assertFalse(borrow1.equals(borrow2));
 
         borrow2.setId(1);
 
         // only id is the same
+        // this kind of instance shouldn't exist!
         assertFalse(borrow1.equals(borrow2));
         assertFalse(borrow2.equals(borrow1));
 
+        
         borrow2.setCustomer(customer1);
         borrow2.setCd(cd1);
         borrow2.setActive(true);
         borrow2.setId(2);
 
-        // id are different
+        // id is different, others are the same
         assertFalse(borrow1.equals(borrow2));
         assertFalse(borrow2.equals(borrow1));
 
@@ -49,4 +48,5 @@ public class BorrowTest {
         assertTrue(borrow1.equals(borrow2));
         assertTrue(borrow2.equals(borrow1));
     }
+
 }
---
+++ tests/cz/muni/fi/pv168/BorrowsImplTest.java
@@ -0,0 +1,56 @@
+package cz.muni.fi.pv168;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: fivekeyem
+ * Date: 3/16/11
+ */
+public class BorrowsImplTest {
+
+    private Borrows borrows;
+    
+    @Before
+    public void setUp() {
+        borrows = new BorrowsImpl();
+    }
+
+    @Test
+    public void testLend() {
+
+        try {
+            borrows.lend(null);
+            fail();
+        } catch (NullPointerException ex) {}
+        catch (Exception ex) {
+            fail();
+        }
+
+    }
+
+    @Test
+    public void testPutBack() {
+
+    }
+
+    @Test
+    public void testGetAllBorrows() {
+
+    }
+
+    @Test
+    public void testGetAllActive() {
+
+    }
+
+
+    private Borrow createSampleBorrow() {
+        CD cd = new CD(1, "The Album", 2011);
+        Customer customer = new Customer(1, "User Name");
+        return new Borrow(1, cd, customer, true);
+    }
+}
