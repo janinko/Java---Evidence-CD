1485d4bc25b92e0790e90608b56a8738abb60096
5Keyem <5Keyem@gmail.com>
2011-03-17 09:11:44 +0100

    extension BorrowImpTest

--- tests/cz/muni/fi/pv168/BorrowsImplTest.java
+++ tests/cz/muni/fi/pv168/BorrowsImplTest.java
@@ -13,15 +13,17 @@ import static junit.framework.Assert.*;
 public class BorrowsImplTest {
 
     private Borrows borrows;
+    private BorrowManager manager;
     
     @Before
     public void setUp() {
         borrows = new BorrowsImpl();
+        manager = new BorrowManagerImpl();
     }
 
     @Test
     public void testLend() {
-
+        
         try {
             borrows.lend(null);
             fail();
@@ -34,6 +36,77 @@ public class BorrowsImplTest {
 
     @Test
     public void testPutBack() {
+        Borrow borrow1 = createSampleBorrow1();
+        Borrow borrow2;
+
+
+        // when we try to put back null, we should get an exception
+        try {
+            borrows.putBack(null);
+            fail();
+        } catch (NullPointerException ex) {}
+        catch (Exception ex) {
+            fail();
+        }
+        
+        // when we try to put back a borrow with false active, we should get an exception
+        borrow1.setActive(false);
+        try {
+            borrows.putBack(borrow1);
+            fail();
+        } catch (IllegalArgumentException ex) {}
+        catch (Exception ex) {
+            fail();
+        }
+
+        // when we try to put back a borrow with null CD, we should get an exception
+        borrow1 = createSampleBorrow1();
+        borrow1.setCd(null);
+        try {
+            borrows.putBack(borrow1);
+            fail();
+        } catch (IllegalArgumentException ex) {}
+        catch (Exception ex) {
+            fail();
+        }
+
+        // when we try to put back a borrow with null Customer, we should get an exception
+        borrow1 = createSampleBorrow1();
+        borrow1.setCustomer(null);
+        try {
+            borrows.putBack(borrow1);
+            fail();
+        } catch (IllegalArgumentException ex) {}
+        catch (Exception ex) {
+            fail();
+        }
+
+
+        // this is ok
+        borrow1 = createSampleBorrow1();
+        manager.createBorrow(borrow1);
+
+        try {
+            borrows.putBack(borrow1);
+        } catch (Exception ex) {
+            fail();
+        }
+
+        // when we try to put back a borrow that isn't contained in manager, we should get an exception
+        borrow1 = createSampleBorrow1();
+        manager.createBorrow(borrow1);
+        borrow2 = createSampleBorrow2();
+
+        try {
+            borrows.putBack(borrow2);
+            fail();
+        } catch (IllegalArgumentException ex) {}
+        catch (Exception ex) {
+            fail();
+        }
+        
+
+        
 
     }
 
@@ -48,10 +121,16 @@ public class BorrowsImplTest {
     }
 
 
-    private Borrow createSampleBorrow() {
-        CD cd = new CD(1, "The Album", 2011);
-        Customer customer = new Customer(1, "User Name");
+    private Borrow createSampleBorrow1() {
+        CD cd = new CD(1, "The Album 1", 2011);
+        Customer customer = new Customer(1, "User Name 1");
         return new Borrow(1, cd, customer, true);
+    }
+
+    private Borrow createSampleBorrow2() {
+        CD cd = new CD(2, "The Album 2", 2011);
+        Customer customer = new Customer(2, "User Name 2");
+        return new Borrow(2, cd, customer, true);
     }
     
 }
