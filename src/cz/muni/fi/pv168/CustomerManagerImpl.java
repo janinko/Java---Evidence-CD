2987dc15916d28feea013cfc53633e1fb8a56a24
5Keyem <5Keyem@gmail.com>
2011-03-28 10:38:25 +0200

    work with database in customer (getAllCustomers)

--- src/cz/muni/fi/pv168/CustomerManagerImpl.java
+++ src/cz/muni/fi/pv168/CustomerManagerImpl.java
@@ -1,6 +1,15 @@
 package cz.muni.fi.pv168;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by IntelliJ IDEA.
@@ -8,6 +17,24 @@ import java.util.SortedSet;
  * Date: 3/14/11
  */
 public class CustomerManagerImpl implements CustomerManager {
+
+    private Connection conn;
+    private static final Logger logger = Logger.getLogger(
+        CustomerManagerImpl.class.getName());
+
+
+    public CustomerManagerImpl() {
+        try {
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/evidencedb", "evname", "evpass");
+            System.out.println("jsem ready");
+
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Error when connecting to DB", ex);
+        }
+
+        
+    }
+
     public Customer createCustomer(Customer customer) {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
@@ -21,7 +48,32 @@ public class CustomerManagerImpl implements CustomerManager {
     }
 
     public SortedSet<Customer> getAllCustomers() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("SELECT * from CUSTOMERS");
+            ResultSet rs = st.executeQuery();
+            SortedSet<Customer> allCustomers = new TreeSet<Customer>();
+
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getInt("id"));
+                customer.setName(rs.getString("name"));
+                allCustomers.add(customer);
+            }
+            return Collections.unmodifiableSortedSet(allCustomers);
+
+        } catch (SQLException ex) {
+            logger.log(Level.SEVERE, "Error when getting all customers from DB", ex);
+            throw new RuntimeException("Error when getting all customers from DB", ex);
+        } finally {
+            if (st != null) {
+                try {
+                    st.close();
+                } catch (SQLException ex) {
+                    logger.log(Level.SEVERE, "Error when closing connection", ex);
+                }
+            }
+        }
     }
 
     public Customer getCustomerById(int id) {
--- src/cz/muni/fi/pv168/Demo.java
+++ src/cz/muni/fi/pv168/Demo.java
@@ -1,7 +1,5 @@
 package cz.muni.fi.pv168;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,13 +13,8 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        Calendar from1 = new GregorianCalendar(2011, 4, 22);
-        Calendar to1 = new GregorianCalendar(2011, 4, 23);
+        CustomerManager cm = new CustomerManagerImpl();
 
-        if (from1.equals(to1)) {
-            System.out.println("rovna se");
-        }
-
-        System.out.println(from1.get(Calendar.DATE));
+        cm.getAllCustomers();
     }
 }
